ade587d077d053947be0eeda22feff4f49b4b48f
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2024-06-19 23:03:09 +0900

    "nw_instanceの機能追加するために新規ディレクトリおよびファイルを作成"

---
+++ tests/unit/test_nw_instance/test_nw_instance.py
@@ -0,0 +1,12 @@
+from noa.network_functions.nw_instance.extract.nw_instance import nw_instance
+from tests.genr.mods.nw_instance.genr_nw_instance import  genr_nw_instance
+
+
+def test_nw_instance(conf):
+  """
+  nw_instance関数をテストする関数
+  args:
+    conf:func
+      testデータの元となるデータを取得し、必要な情報を抽出するための関数
+  """
+  
