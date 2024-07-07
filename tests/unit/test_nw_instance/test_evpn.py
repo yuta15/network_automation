302c212e269ead1409a75a203b1582be402605bc
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2024-07-07 16:14:36 +0900

    Openconfigディレクトリ作成。nw-instance各種ファイル作成

---
+++ tests/unit/test_nw_instance/test_evpn.py
@@ -0,0 +1,15 @@
+
+
+def test_evpn(conf):
+    """
+    nw-instanceのevpnをテストするための関数。
+    example_network-instance.jsonから抽出し、返り値が想定通りであることを確認する。
+    {
+        name: str,
+        type: str,
+        description: str,
+        router-id: str,
+        route-distinguisher: str,
+    }
+    """
+    
